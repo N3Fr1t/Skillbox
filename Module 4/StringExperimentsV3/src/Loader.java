80f0b4639a3b12d0e6062802aef17233b8517527
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2022-11-10 23:49:00 +0300

    Add Loader.java | StringExperimentsV3 homework done

---
+++ Module 4/StringExperimentsV3/src/Loader.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Loader {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ваше ФИО: ");
+
+        String name = scanner.nextLine();
+        name = name.trim();
+
+        int wordAmount = name.split(" +").length;
+        int firstSpaceIndex = name.indexOf(' ');
+        int secondSpaceIndex = name.lastIndexOf(' ');
+
+        if (wordAmount != 3) {
+            System.out.println("ФИО введено неверно");
+        } else if (!checkWords(name)) {
+            System.out.println("ФИО может содержать только буквы");
+        } else {
+            System.out.println("Фамилия: " + name.substring(0, firstSpaceIndex).trim());
+            System.out.println("Имя: " + name.substring(firstSpaceIndex, secondSpaceIndex).trim());
+            System.out.println("Отчество: " + name.substring(secondSpaceIndex).trim());
+        }
+    }
+
+    public static boolean checkWords(String name) {
+        return name.replaceAll(" ", "").matches("[a-zA-Zа-яА-Я]+");
+    }
+}
