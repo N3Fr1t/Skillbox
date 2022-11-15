8ac7dbde5e8f7765d9315a2076c64d3288823949
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2022-11-15 19:31:37 +0300

    Add Main.java | Collection_Of_Emails homework done

---
+++ Module 5/Collection_Of_Emails/src/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Email email = new Email();
+
+        email.start();
+    }
+}
