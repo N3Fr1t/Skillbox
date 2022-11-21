96dfaa0d00de7c04b133bc0ce70399aea5c50aee
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2022-11-21 21:49:13 +0300

    Add BankAccount.java

---
+++ Module 6/Bank/src/BankAccount/BankAccount.java
@@ -0,0 +1,32 @@
+package BankAccount;
+
+import java.text.DecimalFormat;
+
+public class BankAccount {
+    protected double moneyAmount;
+    protected double currentMoneyAmount;
+
+    public BankAccount() {
+    }
+
+    public void setMoneyAmount(double moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
+
+    public void depositMoney() {
+        currentMoneyAmount += moneyAmount;
+    }
+
+    public void withdrawMoney() {
+        if (moneyAmount > currentMoneyAmount) {
+            System.out.println("Недостаточно средств для снятия наличных");
+            checkBalance();
+        } else {
+            currentMoneyAmount -= moneyAmount;
+        }
+    }
+
+    public void checkBalance() {
+        System.out.println("На Вашем счете " + new DecimalFormat("#0.00").format(currentMoneyAmount) + "руб.");
+    }
+}
