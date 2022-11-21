6a8808b23698ea1867f96e80bcda5589758182be
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2022-11-22 02:27:07 +0300

    Main.java updated || extra homework done

--- Module 6/Bank/src/Main.java
+++ Module 6/Bank/src/Main.java
@@ -6,39 +6,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         BankAccount bankAccount = new CardAccount();
         System.out.println("Список доступных операций:" +
-                "\n\t* внести" +
-                "\n\t* снять" +
-                "\n\t* баланс" +
-                "\n\t* выйти");
+                "\n\tВНЕСТИ\t\t - позволяет внести средства на счет" +
+                "\n\tСНЯТЬ\t\t - позволяет снять средства со счета" +
+                "\n\tПЕРЕВЕСТИ\t - позволяет перевести средства на другой счет" +
+                "\n\tБАЛАНC\t\t - позволяет узнать баланс счета" +
+                "\n\tВЫЙТИ");
 
         start(bankAccount);
     }
 
-    public static void start(BankAccount bankAccount) throws IOException, InterruptedException {
+    private static void start(BankAccount bankAccount) throws IOException {
         String operation = "";
         while (!operation.equals("выйти")) {
-            System.out.print("Выберите операцию: ");
-            operation = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            System.out.print("\nВыберите операцию: ");
+            operation = new BufferedReader(new InputStreamReader(System.in)).readLine().toLowerCase();
 
-            if (!operation.equalsIgnoreCase("баланс") && (operation.equalsIgnoreCase("внести") || operation.equalsIgnoreCase("снять"))) {
-                System.out.print("Какую сумму Вы хотите " + operation + "? ");
-                bankAccount.setMoneyAmount(Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine().replaceAll("\\D^.", "")));
-
-                if (operation.equalsIgnoreCase("внести")) {
-                    bankAccount.depositMoney();
-                } else if (operation.equalsIgnoreCase("снять")) {
-                    bankAccount.withdrawMoney();
-                }
-            } else if (operation.equalsIgnoreCase("баланс")) {
-                bankAccount.checkBalance();
-            } else {
-                System.out.println("Введена неверная операция");
-                Thread.sleep(1500);
-            }
-            System.out.println();
+            operationCheck(bankAccount, operation);
         }
+    }
+
+    private static void operationCheck(BankAccount bankAccount, String operation) throws IOException {
+        if (!operation.equals("баланс") && (operation.equals("внести") || operation.equals("снять"))) {
+           moneyManipulation(bankAccount, operation);
+
+           depositOrWithdraw(bankAccount, operation);
+        } else if (operation.equals("баланс")) {
+            bankAccount.checkBalance();
+        } else if (operation.equals("перевести")) {
+            System.out.print("Какую сумму Вы хотите перевести? ");
+            sendMoney(bankAccount);
+        } else
+            System.out.println("Неверная операция");
+    }
+
+    private static void moneyManipulation(BankAccount bankAccount, String operation) throws IOException {
+        System.out.print("Какую сумму Вы хотите " + operation + "? ");
+        bankAccount.setMoneyAmount(readerDouble());
+    }
+
+    private static void depositOrWithdraw(BankAccount bankAccount, String operation) {
+        if (operation.equals("внести")) {
+            bankAccount.depositMoney();
+        } else {
+            bankAccount.withdrawMoney();
+        }
+    }
+
+    private static boolean isSend(BankAccount bankAccount) throws IOException {
+        return bankAccount.send(new BankAccount(), readerDouble());
+    }
+
+    private static void sendMoney(BankAccount bankAccount) throws IOException {
+        if (isSend(bankAccount)) {
+            System.out.println("Денежные средства успешно переведены");
+        } else {
+            System.out.println("На Вашем счете недостаточно средств");
+        }
+    }
+
+    private static double readerDouble() throws IOException {
+        return Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine().replaceAll("\\D", ""));
     }
 }
