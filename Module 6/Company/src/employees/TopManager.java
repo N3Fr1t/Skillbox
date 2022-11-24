5791e96ee9012a416f0299229cbd306ebec27c85
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2022-11-24 19:19:31 +0300

    Add TopManager.java

--- Module 6/Company/src/employees/TopManager.java
+++ Module 6/Company/src/employees/TopManager.java
@@ -1,2 +1,10 @@
-package employees;public class TopManager {
+package employees;
+
+public class TopManager implements Employee {
+    private static final int FIXED_SALARY = 120000;
+
+    @Override
+    public int getMonthSalary() {
+        return (FIXED_SALARY + (Company.getIncome() > 1000000 ? (int) (FIXED_SALARY * 1.5) : 0));
+    }
 }
