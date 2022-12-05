4038210ae3280c1c67133a543f1d1e7e0fcf70d1
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2022-12-05 12:20:15 +0300

    TopManager.java | overriden toString method

--- Module 6/Company/src/employees/TopManager.java
+++ Module 6/Company/src/employees/TopManager.java
@@ -1,10 +1,19 @@
 package employees;
 
 public class TopManager implements Employee {
-    private static final int FIXED_SALARY = 120000;
-
     @Override
     public int getMonthSalary() {
-        return (FIXED_SALARY + (Company.getIncome() > 10000000 ? (int) (FIXED_SALARY * 1.5) : 0));
+        int FIXED_SALARY = 150000;
+
+        if (Company.getIncome() > 10000000) {
+            return FIXED_SALARY + (int) (FIXED_SALARY * 1.5);
+        } else
+            return 150000;
+    }
+
+    @Override
+    public String toString() {
+        return "ТИП: топ менеджер" +
+                "\n\tЗ/П " + getMonthSalary() + " руб.";
     }
 }
