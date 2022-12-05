57e9c95a7c1b685330060da116d84d210299a8d3
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2022-12-05 12:23:31 +0300

    Add Main.java | fixed bugs | Company homework done

--- Module 6/Company/src/Main.java
+++ Module 6/Company/src/Main.java
@@ -1,58 +1,76 @@
 import employees.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
         Company company = new Company();
 
-        hireEmployees(company);
-        printHighestSalaries(company);
-        printLowestSalaries(company);
+        System.out.println("Доход компании составил " + Company.getIncome() + " руб.\n");
+
+        hireEmployee(company);
+        hireAllEmployee(company);
+        System.out.println("Всего в компании " + company.getEmployeesCount() + " сотрудников\n");
+
+        printTopSalary(company);
+        printLowestSalary(company);
+
         fireHalfEmployees(company);
-        printHighestSalaries(company);
-        printLowestSalaries(company);
+        printTopSalary(company);
+        printLowestSalary(company);
     }
 
-    public static void hireEmployees(Company company) {
-        for (int i = 0; i < 180; i++) {
-            company.hire(new Operator());
-        }
+    public static void hireEmployee(Company company) {
+        int employeesCount = company.getEmployeesCount();
+        company.hire(new Operator());
+        company.hire(new Manager());
+        company.hire(new TopManager());
 
-        for (int i = 0; i < 80; i++) {
-            company.hire(new Manager());
-        }
-
-        for (int i = 0; i < 10; i++) {
-            company.hire(new TopManager());
-        }
-        System.out.println("Добавлено " + company.countEmployees() + " сотрудников");
-        System.out.println("============================");
+        System.out.println("Добавлено " + (company.getEmployeesCount() - employeesCount) + " сотрудников");
     }
 
-    private static void printHighestSalaries(Company company) {
+    public static void hireAllEmployee(Company company) {
+        Collection<Employee> employees = new ArrayList<>();
+        int employeesCount = company.getEmployeesCount();
+
+        for (int i = 0; i < 179; i++) {
+            employees.add(new Operator());
+        }
+
+        for (int i = 0; i < 79; i++) {
+            employees.add(new Manager());
+        }
+
+        for (int i = 0; i < 9; i++) {
+            employees.add(new TopManager());
+        }
+        company.hireAll(employees);
+
+        System.out.println("Добавлено " + (company.getEmployeesCount() - employeesCount) + " сотрудников");
+    }
+
+    public static void printTopSalary(Company company) {
         System.out.println("Список самых высоких зарплат");
         for (Employee employee : company.getTopSalaryStaff(15)) {
-            System.out.println(employee.getMonthSalary());
+            System.out.println(employee.getMonthSalary() + " руб.");
         }
-        System.out.println("============================");
+        System.out.println();
     }
 
-    private static void printLowestSalaries(Company company) {
+    public static void printLowestSalary(Company company) {
         System.out.println("Список самых низких зарплат");
         for (Employee employee : company.getLowestSalaryStaff(30)) {
-            System.out.println(employee.getMonthSalary());
+            System.out.println(employee.getMonthSalary() + " руб.");
         }
-        System.out.println("============================");
     }
 
-    private static void fireHalfEmployees(Company company) {
-        int firedEmployeesCount = company.countEmployees();
+    public static void fireHalfEmployees(Company company) {
+        int firedEmployeesCount = company.getEmployeesCount();
 
-        for (int i = 0; i < company.countEmployees() / 2; i++) {
-            int index = (int) (Math.random() * company.countEmployees());
-            Employee firedEmployee = company.getEmployees().get(index);
-            company.fire(firedEmployee);
+        for (int i = 0; i < company.getEmployeesCount() / 2; i++) {
+            company.fire(company.getEmployees().get((int) (Math.random() * company.getEmployeesCount())));
         }
-        System.out.println("Уволено " + firedEmployeesCount / 2  + " сотрудников");
-        System.out.println("============================");
+        System.out.println("\nУволено " + firedEmployeesCount / 2 + " сотрудников\n");
     }
 }
